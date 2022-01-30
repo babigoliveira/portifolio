ec6ab271259df80226a87cc6b3308192855c6557
Bárbara Oliveira <barbaragdeoliveira@hotmail.com>
2022-01-30 13:36:30 -0300

    add dynamic repositories

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-$(document).ready(function () {
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-  });
-
-  new Typed(".typing", {
-    strings: ["Developer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  new Typed(".typing-2", {
-    strings: ["Developer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-
+function setupCarousel() {
   $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
@@ -42,6 +19,31 @@ $(document).ready(function () {
       },
     },
   });
+}
+
+function configurePageLayout() {
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  new Typed(".typing", {
+    strings: ["Desenvolvedora", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  new Typed(".typing-2", {
+    strings: ["Desenvolvedora", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
   $(window).scroll(function () {
     if (this.scrollY > 20) {
@@ -56,4 +58,50 @@ $(document).ready(function () {
       $(".scroll-up-btn").removeClass("show");
     }
   });
+}
+
+function loadCarouselScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  script.onload = setupCarousel;
+  document.head.appendChild(script);
+}
+
+function loadRepos() {
+  $.ajax({
+    url: "https://api.github.com/users/babigoliveira/repos",
+    dataType: "json",
+    async: true,
+  }).then((repos) => {
+    repos = repos.map(({ description, html_url, name }) => ({
+      description,
+      html_url,
+      name,
+    }));
+
+    const html = repos
+      .map(
+        (repo) => `
+          <div class="card">
+            <div class="box">
+              <img src="images/github-light.png" alt="">
+              <div class="text">${repo.name}</div>
+              <p>${repo.description}</p>
+            </div>
+          </div>`
+      )
+      .join("");
+
+    document.querySelector("#projects").innerHTML = html;
+
+    loadCarouselScript();
+  });
+}
+
+$(document).ready(function () {
+  configurePageLayout();
+  loadRepos();
 });
