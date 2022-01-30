e435ef58fb8ed737e3544df0ad0f392866640159
Bárbara Oliveira <barbaragdeoliveira@hotmail.com>
2022-01-30 17:47:30 -0300

    submit form

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+function submitForm() {
+  const name = document.getElementById("name")?.value;
+  const email = document.getElementById("email")?.value;
+  const subject = document.getElementById("subject")?.value;
+  const message = document.getElementById("message")?.value;
+
+  if (!(name && email && subject && message)) {
+    return;
+  }
+
+  const formattedMessage = `
+    *Nome*: ${name}
+    *Email*: ${email}
+    *Assunto*: ${subject}
+    *Mensagem*: ${message}
+  `.replaceAll(/\n +/g, "\n");
+
+  const url =
+    "https://api.whatsapp.com/send?phone=+5531985972616&text=" +
+    encodeURIComponent(formattedMessage);
+
+  window.open(url, "_blank");
+}
+
+function validaFormulario() {
+  const nome = document.getElementById("nome");
+  const email = document.getElementById("email");
+  const assunto = document.getElementById("assunto");
+  const mensagem = document.getElementById("mensagem");
+
+  const message = `
+    \`\`\`
+    Nome: ${nome}
+    Email: ${email}
+    Assunto: ${assunto}
+    Mensagem: ${mensagem}
+    \`\`\`
+  `.replace(/^ +/g, "");
+
+  const url =
+    "https://api.whatsapp.com/send?phone=+5531985972616&text=" + message;
+
+  document.querySelector("#sendFormButton").href = url;
+}
+
 function setupCarousel() {
   $(".carousel").owlCarousel({
     margin: 20,
@@ -102,6 +147,7 @@ function loadRepos() {
 }
 
 $(document).ready(function () {
+  $("#sendFormButton").on("click", submitForm);
   configurePageLayout();
   loadRepos();
 });
